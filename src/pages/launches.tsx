eb651ee7358805af5eee2a94308052e30d6678ae
RaymondMwaura <raywachaga@gmail.com>
2020-03-04 21:15:51 +0200

    First commit

---
+++ src/pages/launches.tsx
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react';
+import { RouteComponentProps } from '@reach/router';
+
+type LaunchesProps = RouteComponentProps
+
+const Launches: React.FC<LaunchesProps> = () => <div />;
+
+export default Launches;
